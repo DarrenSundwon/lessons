88a7569257e3ad5972c4b3a1fa7cf8e5d0d75892
Darren_Sundown <brattsevyuriy@gmail.com>
2015-09-03 19:59:23 +0300

    First contact

--- index.php
+++ index.php
@@ -2,10 +2,16 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Document</title>
+    <title>Test page</title>
 </head>
 <body>
     <h1>Site name</h1>
     <p>Hello world!</p>
+    <?php
+        echo 'it`s a Darren Sundown';
+        while ($x < 10) {
+            echo 'Hell yeah!!!';
+        }
+    ?>
 </body>
 </html>
